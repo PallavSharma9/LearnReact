6fa2fce9412e873a7394d2b35aa97bcddf9b2488
Pallav Sharma <pallavsharma0202@gmail.com>
2023-10-26 16:34:40 +0530

    Doing LearnRecoil project using contextApi

--- LearnRecoil/src/App.jsx
+++ LearnRecoil/src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from 'react'
+import { useContext, useState, createContext } from 'react'
 
+
+// now doing by context api
+
+const CountContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div style={{width: "100vw", height: "100vh", backgroundColor: "skyblue", display: "flex", justifyContent: "center" }}>
-      <div style={{width: "500px",height: "200px", backgroundColor: "white"}}>
-        <h1 style={{textAlign: "center", maringBottom: "10px", color: "black", fontSize: "20px"}}>Counter app</h1>
-        <Buttons count={count} setCount={setCount} />
-        <CountComponent count={count} />
-      </div>
-    </div>
-
+    <CountContext.Provider value={{
+      count: count,
+      setCount: setCount
+    }}>
+      <div style={{width: "100vw", height: "100vh", backgroundColor: "skyblue", display: "flex", justifyContent: "center" }}>
+       <div style={{width: "500px",height: "200px", backgroundColor: "white"}}>
+          <h1 style={{textAlign: "center", maringBottom: "10px", color: "black", fontSize: "20px"}}>Counter app</h1>
+          <Buttons />
+          <CountComponent />
+        </div>
+      </div>    
+    </CountContext.Provider>
   )
 }
 
-function Buttons({count, setCount}){
+function Buttons(){
     return <div style={{display: "flex", justifyContent: "space-between"}}>
-      <Increase count={count} setCount={setCount} />
-      <Decrease  count={count} setCount={setCount} />
+      <Increase/>
+      <Decrease/>
     </div>
 }
 
-function Increase({count, setCount}){
+function Increase(){
+  const {count, setCount} = useContext(CountContext)
   return <div>
     <button onClick={()=>{
       setCount(count+1)
@@ -33,7 +42,8 @@ function Increase({count, setCount}){
   </div>
 }
 
-function Decrease({count, setCount}){
+function Decrease(){
+  const {count, setCount} = useContext(CountContext)
   return <div>
     <button onClick={()=>{
       setCount(count-1)
@@ -42,7 +52,8 @@ function Decrease({count, setCount}){
     </button>
   </div>
 }
-function CountComponent({count}) {
+function CountComponent() {
+  const {count} = useContext(CountContext)
   return  <div>
     <h1 style={{textAlign: "center", marginTop: "10px", color: "black"}}>{count}</h1>
   </div>
